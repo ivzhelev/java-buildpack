39057c01ce51c3b424c7cade8033712b7694a8be
Ivaylo <ivaylo.zhelev@gmail.com>
2026-01-12 14:29:29 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,26 +14,30 @@ function main() {
   util::tools::jq::install --directory "${ROOTDIR}/.bin"
 
   IFS=" " read -r -a oses <<< "$(jq -r -S '.oses[]' "${ROOTDIR}/config.json" | xargs)"
-  
+  IFS=" " read -r -a arches <<< "$(jq -r -S '.arches[]' "${ROOTDIR}/config.json" | xargs)"
+
   mapfile -t binaries < <(find "${ROOTDIR}/src" -mindepth 2 -name cli -type d)
 
   for os in "${oses[@]}"; do
-    for path in "${binaries[@]}"; do
-      local name output
-      name="$(basename "$(dirname "${path}")")"
-      output="${ROOTDIR}/bin/${name}"
+    for arch in "${arches[@]}"; do
+      for path in "${binaries[@]}"; do
+        local name output
+        name="$(basename "$(dirname "${path}")")"
+        output="${ROOTDIR}/bin/${name}"
 
-      if [[ "${os}" == "windows" ]]; then
-        output="${output}.exe"
-      fi
+        if [[ "${os}" == "windows" ]]; then
+          output="${output}.exe"
+        fi
 
-      CGO_ENABLED=0 \
-      GOOS="${os}" \
-        go build \
-          -mod vendor \
-          -ldflags="-s -w" \
-          -o "${output}" \
-            "${path}"
+        CGO_ENABLED=0 \
+        GOOS="${os}" \
+        GOARCH="${arch}" \
+          go build \
+            -mod vendor \
+            -ldflags="-s -w" \
+            -o "${output}" \
+              "${path}"
+      done
     done
   done
 }
