80d245a5ff0120a52bd806da2f403396604463ef
Ivaylo <ivaylo.zhelev@gmail.com>
2026-01-12 15:10:27 +0200

    Refactor build script to use first architecture only

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,30 +14,29 @@ function main() {
   util::tools::jq::install --directory "${ROOTDIR}/.bin"
 
   IFS=" " read -r -a oses <<< "$(jq -r -S '.oses[]' "${ROOTDIR}/config.json" | xargs)"
-  IFS=" " read -r -a arches <<< "$(jq -r -S '.arches[]' "${ROOTDIR}/config.json" | xargs)"
+  local arch
+  arch="$(jq -r '.arches[0]' "${ROOTDIR}/config.json")"
 
   mapfile -t binaries < <(find "${ROOTDIR}/src" -mindepth 2 -name cli -type d)
 
   for os in "${oses[@]}"; do
-    for arch in "${arches[@]}"; do
-      for path in "${binaries[@]}"; do
-        local name output
-        name="$(basename "$(dirname "${path}")")"
-        output="${ROOTDIR}/bin/${name}"
+    for path in "${binaries[@]}"; do
+      local name output
+      name="$(basename "$(dirname "${path}")")"
+      output="${ROOTDIR}/bin/${name}"
 
-        if [[ "${os}" == "windows" ]]; then
-          output="${output}.exe"
-        fi
+      if [[ "${os}" == "windows" ]]; then
+        output="${output}.exe"
+      fi
 
-        CGO_ENABLED=0 \
-        GOOS="${os}" \
-        GOARCH="${arch}" \
-          go build \
-            -mod vendor \
-            -ldflags="-s -w" \
-            -o "${output}" \
-              "${path}"
-      done
+      CGO_ENABLED=0 \
+      GOOS="${os}" \
+      GOARCH="${arch}" \
+        go build \
+          -mod vendor \
+          -ldflags="-s -w" \
+          -o "${output}" \
+            "${path}"
     done
   done
 }
